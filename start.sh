ca376d4c2cd09697b02fddf1ae5dff50fd8b8996
spider <spidertest009@gmail.com>
2025-11-17 14:38:28 +0530

    updated New 6

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-#!/bin/bash
-#!/bin/bash
-gunicorn app:app -k eventlet --worker-connections 1000 --timeout 0 --bind 0.0.0.0:$PORT
+gunicorn app:app --worker-class eventlet -w 1 --bind 0.0.0.0:$PORT
